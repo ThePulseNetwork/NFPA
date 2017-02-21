d2d97a91cfb424debb700ff83d10a08242c131c9
Accounts TPN <accounts@thepulsenetwork.com>
2017-02-21 08:23:04 -0500

    Single Category Page

--- NFPA/components/SubCategoryView/index.js
+++ NFPA/components/SubCategoryView/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-app.category05View = kendo.observable({
+app.subCategoryView = kendo.observable({
     onShow: function() {},
     afterShow: function() {}
 });
-app.localization.registerView('category05View');
+app.localization.registerView('subCategoryView');
 
-// START_CUSTOM_CODE_category05View
+// START_CUSTOM_CODE_subCategoryView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
-// END_CUSTOM_CODE_category05View
+// END_CUSTOM_CODE_subCategoryView
